da3ab8cf394f08bd6159c041eb0511af80ebc309
Dan Ravensloft <dan.ravensloft@gmail.com>
2017-12-02 21:09:17 +0000

    Add device string array

--- 0e01.py
+++ 0e01.py
@@ -1,5 +1,34 @@
 #!/usr/bin/python3
 
+devices = {0x40: "K63",
+            0x17: "K65 RGB",
+            0x07: "K65",
+            0x37: "K65 LUX RGB",
+            0x39: "K65 RAPIDFIRE",
+            0x3f: "K68 RGB",
+            0x13: "K70 RGB",
+            0x09: "K70",
+            0x33: "K70 LUX RGB",
+            0x36: "K70 LUX",
+            0x38: "K70 RAPIDFIRE RGB",
+            0x3a: "K70 RAPIDFIRE",
+            0x11: "K95 RGB",
+            0x08: "K95",
+            0x2d: "K95 PLATINUM RGB",
+            0x20: "STRAFE RGB",
+            0x15: "STRAFE",
+            0x44: "STRAFE",
+            0x12: "M65 RGB",
+            0x2e: "M65 PRO RGB",
+            0x14: "SABRE RGB", # Optical
+            0x19: "SABRE RGB", # Laser
+            0x2f: "SABRE RGB", # New?
+            0x32: "SABRE RGB", # Alternate
+            0x1e: "SCIMITAR RGB",
+            0x3e: "SCIMITAR PRO RGB",
+            0x3c: "HARPOON RGB",
+            0x34: "GLAIVE RGB"}
+
 packet = [int("0x" + s, 16) for s in input().split(" ")]
 
 if packet[0] != 0x0e or packet[1] != 0x01:
@@ -18,7 +47,7 @@ else:
 print("Firmware: %d.0%d" % (packet[9], packet[8])) 
 print("Bootloader: %d%d" % (packet[11], packet[10]))
 print("Vendor: %x%x" % (packet[13], packet[12]))
-print("Product: %x%x" % (packet[15], packet[14]))
+print("Product: %x%x (%s)" % (packet[15], packet[14], devices[packet[14]]))
 print("Poll rate: %dms" % (packet[16]))
 
 # packets 17 to 19 are unknown
