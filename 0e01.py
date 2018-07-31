a67a772263b28665d3fb1350febd7d9b758b5478
Dan Ravensloft <dan.ravensloft@gmail.com>
2018-07-31 23:35:02 +0100

    Update 0e01 parser

--- 0e01.py
+++ 0e01.py
@@ -10,12 +10,14 @@ devices = {0x40: "K63",
             0x09: "K70",
             0x33: "K70 LUX RGB",
             0x36: "K70 LUX",
+            0x48: "K70 RGB MK.2",
             0x38: "K70 RAPIDFIRE RGB",
             0x3a: "K70 RAPIDFIRE",
             0x11: "K95 RGB",
             0x08: "K95",
             0x2d: "K95 PLATINUM RGB",
             0x20: "STRAFE RGB",
+            0x49: "STRAFE RGB MK.2",
             0x15: "STRAFE",
             0x44: "STRAFE",
             0x12: "M65 RGB",
@@ -41,14 +43,9 @@ if packet[0] != 0x0e or packet[1] != 0x01:
 
 # packet[2] and [3] are ignored
 
-if packet[4]:
-    print("Lighting: 24 bit")
-else:
-    print("Lighting: 9 bit")
-
 # packets 5 to 7 are unknown
 
-print("Firmware: %d.0%d" % (packet[9], packet[8])) 
+print("Firmware: %d.%d%d" % (packet[9], (packet[8] & 0xF0) >> 4, (packet[8] & 0x0F)))
 print("Bootloader: %d%d" % (packet[11], packet[10]))
 print("Vendor: %x%x" % (packet[13], packet[12]))
 print("Product: %x%x (%s)" % (packet[15], packet[14], devices[packet[14]]))
@@ -58,16 +55,27 @@ print("Poll rate: %dms" % (packet[16]))
 
 if packet[20] == 0xc0:
     print("Type: Keyboard")
+
+    print("Layout: ", end='')
+    layouts = [
+        "ANSI",
+        "ISO",
+        "ABNT",
+        "JIS",
+        "Dubeolsik"
+    ]
+    print(layouts[packet[23]])
 elif packet[20] == 0xc1:
     print("Type: Mouse")
+
+    print("Lighting zones: ", end='')
+    for i in range(8):
+        if (1 << i) & packet[24]:
+            print("%d " % i, end='')
+    print()
 elif packet[20] == 0xc2:
     print("Type: Mousepad")
 else:
     print("Type: Unknown")
     
-print("Lighting zones: ", end='')
-for i in range(8):
-    if (1 << i) & packet[24]:
-        print("%d " % i, end='')
-print()
 
